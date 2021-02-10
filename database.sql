615e7c54fdf9fe78a1880a7dd6c8d1373517820e
Woody Kromar <wkromar12@gmail.com>
2021-02-10 10:43:57 -0600

    working on SQL

--- database.sql
+++ database.sql
@@ -8,3 +8,45 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+CREATE TABLE "user"(
+"id" SERIAL PRIMARY KEY,
+"username" varchar(20) NOT NULL,
+"password" varchar(8) NOT NULL,
+"is_admin" varchar(5) DEFAULT ('false')
+);
+
+INSERT INTO "user" ("username", "password")
+VALUES ('wkromar', 'chungus');
+
+CREATE TABLE "user_snacklist"(
+	"id" SERIAL PRIMARY KEY,
+	"user_snack_id" INT,
+	"user_id" INT,
+	snack_favorite varchar(5) DEFAULT ('false')
+);
+
+CREATE TABLE "snack_list"(
+"id" SERIAL PRIMARY KEY,
+"snack_name" varchar(100),
+"snack_nutrition" 
+)
+
+CREATE TABLE "snack_categories" (
+"id" SERIAL PRIMARY KEY,
+"name" VARCHAR (100) NOT NULL);
+
+
+-- MAKE MOST VALUES NOT NULL AFTER GETTING CONNECTIONS TO API SET UP
+CREATE TABLE "snack_list"(
+"id" SERIAL PRIMARY KEY,
+"snack_name" VARCHAR(100) NOT NULL,
+"snack_nutrition" VARCHAR(500),
+"snack_image" VARCHAR (150),
+"snack_id" INT,
+"category_id" VARCHAR(100));
+
+INSERT INTO "snack_list" ("snack_name","snack_nutrition", "snack_image","snack_id","category_id" ) 
+VALUES ('Cheetos', '100% health', 'Image goes here', '1', '2');
+
+INSERT INTO "snack_categories" ("name")
+VALUES ('salty'), ('cheesey');
